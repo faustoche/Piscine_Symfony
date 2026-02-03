e53bb48033ae22e184fda31c69c39c14b601f47b
faustoche <faustine.crocq@gmail.com>
2026-02-03 10:36:23 +0100

    mendeleiev tab personnalization done

--- Symfony_0_Starting/ex06/mendeleiev.php
+++ Symfony_0_Starting/ex06/mendeleiev.php
@@ -4,7 +4,17 @@ function mendeleiev() {
 
 	$file_content = file_get_contents("ex06.txt");
 	$mendeleiev_tab = explode("\n", $file_content);
-	$html_result = "<table>";
+	$html_result = "
+		<!DOCTYPE html>
+		<html>
+
+		<head>
+			<title>Mendeleiev's table</title>
+		</head>
+
+		<body style=\"background-color: rgb(36, 34, 34); color: white; \">";
+
+	$html_result .= "<table>";
 	$current_col = 0;
 
 	foreach($mendeleiev_tab as $all_elements) {
@@ -31,8 +41,8 @@ function mendeleiev() {
 		$empty_case = $position[1] - $current_col;
 		if ($empty_case > 0)
 			$html_result .= str_repeat("<td></td>", $empty_case);
-		$html_result .= "<td style=\"border: 1px solid black; padding:10px\" background>\n";
-		$html_result .= "<h4 style=\"text-align: center\">" . $elements[0] . "</h4>\n";
+		$html_result .= "<td style=\"border: 1px solid #94206dff; padding:10px; box-shadow: 0 0 15px #ff00aa;\" background>\n";
+		$html_result .= "<h4 style=\"text-align: center; color: #ff00aa\">" . $elements[0] . "</h4>\n";
 		$html_result .= "<ul>\n";
 		$html_result .= "<li>" . $weight[1] . "</li>\n";
 		$html_result .= "<li>" . $symbol[1] . "</li>\n";
