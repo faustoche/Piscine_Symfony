ccc6793c780bd087a0f7ec464af37c54ff57790b
faustoche <faustine.crocq@gmail.com>
2026-02-09 21:44:29 +0100

    ex08symf2

--- Symfony_20_SQL/ex07/config/bundles.php
+++ Symfony_20_SQL/ex07/config/bundles.php
@@ -2,8 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    App\e06Bundle\e06Bundle::class => ['all' => true],
+    App\e07Bundle\e07Bundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
---
+++ Symfony_20_SQL/ex07/src/e07Bundle/Form/PersonType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\e07Bundle\Form;
+
+use App\e07Bundle\Entity\Person;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType {
+	
+	## pour la contruction du formulaire
+	public function buildForm(FormBuilderInterface $builder, array $options): void {
+		## le builder avec tous les attributs ici 
+
+		$builder
+			->add('username', TextType::class)
+			->add('name', TextType::class)
+			->add('email', TextType::class)
+			->add('enable', CheckboxType::class, ['required' => false])
+			->add('birthdate', DateType::class, [
+				'widget' => 'single_text',
+				'required' => false
+			])
+			->add('address', TextareaType::class)
+			->add('save', SubmitType::class, ['label' => 'Save']);
+	}
+
+
+	public function configureOptions(OptionsResolver $resolver): void {
+		$resolver->setDefaults([
+			'data_class' => Person::class,
+		]);
+	}
+}
